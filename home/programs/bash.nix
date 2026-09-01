c8d8d2a43866c589b0c73740d5ddada7a6c90fd8
Enzobrt <BrotonEnzo@gmail.com>
2026-09-01 22:36:15 +0200

    Cambiando alias

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -9,7 +9,7 @@
         sc = "cd /home/enzo/Documents/Programación/scripts/";
         prog ="cd ~/Documents/Programación/ && cd '$(find */ -maxdepth 1 -type d | fzf --cycle)'";
         cole ="cd ~/Documents/Cole/ && cd '$(find */ -maxdepth 1 -type d | fzf --cycle)'";
-        task ="nvim ~/Documents/notas-obsidian/task.md";
+        task ="nvim ~/Documents/notas-obsidian/tasks.md";
         notas = "cd ~/Documents/notas-obsidian/ && nvim '$(find **md  **/*md -type f | fzf --cycle)'";
         empezados =" nvim ~/Documents/notas-obsidian/empezados.md";
       };
