ddd792af9428add00f2d95f55f9973d503bcc0eb
Enzobrt <BrotonEnzo@gmail.com>
2026-09-04 12:04:00 +0200

    He configurado las apps de autostart

--- home/programs/xdg.nix
+++ home/programs/xdg.nix
@@ -1,18 +1,22 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   xdg = {
-  enable = true;
-
-  userDirs = {
     enable = true;
 
-    desktop = "${config.home.homeDirectory}/Desktop";
-    documents = "${config.home.homeDirectory}/Documents";
-    download = "${config.home.homeDirectory}/Downloads";
-    music = "${config.home.homeDirectory}/Music";
-    pictures = "${config.home.homeDirectory}/Pictures";
-    videos = "${config.home.homeDirectory}/Videos";
+    userDirs = {
+      enable = true;
+      desktop = "${config.home.homeDirectory}/Desktop";
+      documents = "${config.home.homeDirectory}/Documents";
+      download = "${config.home.homeDirectory}/Downloads";
+      music = "${config.home.homeDirectory}/Music";
+      pictures = "${config.home.homeDirectory}/Pictures";
+      videos = "${config.home.homeDirectory}/Videos";
     };
+
+    configFile."autostart/vesktop.desktop".source =
+      "${pkgs.vesktop}/share/applications/vesktop.desktop";
+    configFile."autostart/org.gnome.World.PikaBackup.desktop".source =
+      "${pkgs.pika-backup}/share/applications/org.gnome.World.PikaBackup.desktop";
   };
 }
